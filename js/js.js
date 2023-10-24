1e188389bbc23a0d081a3f7b7cb5579b663c739d
Ronddaa <tronddaa@gmail.com>
2023-10-24 12:33:27 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,13 +1,12 @@
-function checkStorage(available, ordered) {
-  let message;
-  // Change code below this line
-if (ordered === 0) {
-  message = 'There are no products in the order!'
-} else if 
-  (ordered > available) { message = 'Your order is too large, there are not enough items in stock'
-} else {
-  message = 'The order is accepted, our manager will contact you'
-}
-  // Change code above this line
-  return message;
-}
+const courseTopic = "JavaScript essentials";
+// Change code below this line
+
+const courseTopicLength = courseTopic;
+const firstElement = courseTopic;
+const lastElement = courseTopic;
+
+console.log(courseTopicLength.length);
+console.log(courseTopicLength[0]);
+console.log(courseTopicLength.length - 1);
+
+// Change code above this line
