f1dfe5f013de28b57fdb24e3aacadc9cb68bf4e2
Ronddaa <tronddaa@gmail.com>
2023-10-25 14:55:58 +0200

    asd

--- js/js.js
+++ js/js.js
@@ -10,3 +10,21 @@ console.log(courseTopicLength[0]);
 console.log(courseTopicLength.length - 1);
 
 // Change code above this line
+
+function getSubstring(string, length) {
+    const substring = getSubstring.slice(getSubstring.length); // Change this line
+
+    return substring;
+}
+
+function formatMessage(message, maxLength) {
+  let result;
+  // Change code below this line
+  if (message <= maxLength) {
+    message
+  } else {
+    message.slice(0, maxLength) + "..."
+  }
+  /// Change code above this line
+  return result;
+}
