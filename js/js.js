7b46d86b967897b3947520c33e3c4096dea8f704
Ronddaa <tronddaa@gmail.com>
2023-10-31 12:23:48 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,14 +1,14 @@
-function checkForName(fullName, firstName) {
-  let message;
-  const fullNameToLow = fullName.toLowerCase();
-  const firstNameToLowCase = firstName.toLowerCase();
-
-  if (fullNameToLow.includes(firstNameToLowCase)) {
-    message = true;
-  } else {
-    message = false;
-
-    return message;
-  }
+function getFileName(file) {
   
+  const dot = 
+  
+  if ()
 }
+
+console.log(getFileName("styles.css"));
+console.log(getFileName("app.js"));
+console.log(getFileName("app"));
+console.log(getFileName("index.js"));
+console.log(getFileName("index.html"));
+console.log(getFileName("index.css"));
+console.log(getFileName("index"));
