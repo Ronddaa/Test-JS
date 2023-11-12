b84b62ab55b596af066eb1cdde1aa312dfb06d48
Ronddaa <tronddaa@gmail.com>
2023-11-12 19:52:20 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,14 +1,15 @@
-function findNumber(start, end, divisor) {
-    let total = 0;
-    for (let i = start; i <= end; i++) {
-        if (i % divisor === 0) {
-            total += i;
-        }
-    }
-    return total;
+function getExtremeElements(array) {
+  
+  const firstElement = array[0]; // Перший елемент масиву
+  const lastElement = array[array.length - 1]; // Останній елемент масиву
+
+  const firstLast = [firstElement, lastElement];
+
+  return firstLast;
 }
 
-console.log(findNumber(8, 17, 3));
-console.log(findNumber(16, 35, 7));
-console.log(findNumber(2, 6, 5));
-console.log(findNumber(6, 9, 4));
+console.log(array[1, 2, 3, 4, 5]);
+console.log(array[2, 3, 4, 5, 6]);
+console.log(array[13, 23, 33, 43, 53]);
+console.log(array[14, 24, 34, 44, 54]);
+console.log(array[145, 245, 345, 445, 55]);
