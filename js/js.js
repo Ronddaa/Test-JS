a4143b3b6d7dad7888e4a768bf0c7aef85e55983
Ronddaa <tronddaa@gmail.com>
2023-11-14 16:04:43 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,15 +1,11 @@
-function getExtremeElements(array) {
-  
-  const firstElement = array[0]; // Перший елемент масиву
-  const lastElement = array[array.length - 1]; // Останній елемент масиву
-
-  const firstLast = [firstElement, lastElement];
-
-  return firstLast;
+function calculateTotal(number) {
+    let sum = 0;
+    let a = 1;
+    
+    while (a <= number) {
+        sum += a;
+        a++;
+    }
+    
+    return sum;
 }
-
-console.log(array[1, 2, 3, 4, 5]);
-console.log(array[2, 3, 4, 5, 6]);
-console.log(array[13, 23, 33, 43, 53]);
-console.log(array[14, 24, 34, 44, 54]);
-console.log(array[145, 245, 345, 445, 55]);
