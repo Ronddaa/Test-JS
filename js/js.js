317f9446a3e392f1c3f8ebcd00375b45a51cc42c
Ronddaa <tronddaa@gmail.com>
2023-11-20 12:16:51 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -134,3 +134,72 @@ console.log(calculateTax(200));
 
 // - Функціональний вираз
 // + Оголошення функції
+
+// Домашняя работа 3 по JS
+
+// Первое задание !!!
+
+function slugify(title) {
+  let address;
+  let slug = title.toLowerCase().split(' ');
+  address = slug.join('-');
+
+  return address;
+
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
+// Второе задание !!!
+
+function makeArray(firstArray, secondArray, maxLength) {
+
+ let mass = firstArray.concat(secondArray);
+  let newMass;
+  if (mass.length > maxLength) {
+    newMass = mass.slice(0, maxLength);
+  } else {
+    newMass = mass;
+  }
+  return newMass;
+
+  // Или сделать так, но по условиям зачем то нужен if ... else
+
+  // let mass = firstArray.concat(secondArray);
+  // let newMass = mass.slice(0, maxLength);
+  // return newMass;
+
+}
+
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+
+// Третье задание !!!
+
+
+function filterArray(numbers, value) {
+  let newMass = [];
+  for (let num of numbers) { 
+    if (num > value) {
+      newMass.push(num);
+    }
+  }
+  return newMass;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
